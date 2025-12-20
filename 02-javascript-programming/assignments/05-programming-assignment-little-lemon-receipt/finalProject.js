035ba734cbbca733352c9d23d6d3d580b67ad6d1
Steve Rocha <steve26rocha@gmail.com>
2025-12-20 07:16:01 +0000

    Complete 05-programming-assignment: Little Lemon Receipt Maker

--- 02-javascript-programming/assignments/05-programming-assignment-little-lemon-receipt/finalProject.js
+++ 02-javascript-programming/assignments/05-programming-assignment-little-lemon-receipt/finalProject.js
@@ -1,51 +1,46 @@
 // Dishes Data
 const dishData = [
-    { name: "Italian pasta", price: 9.55 },
-    { name: "Rice with veggies", price: 8.65 },
-    { name: "Chicken with potatoes", price: 15.55 },
-    { name: "Vegetarian Pizza", price: 6.45 },
+  { name: "Italian pasta", price: 9.55 },
+  { name: "Rice with veggies", price: 8.65 },
+  { name: "Chicken with potatoes", price: 15.55 },
+  { name: "Vegetarian Pizza", price: 6.45 },
 ];
 
 // Tax Value
-const tax = 1.20;
+const tax = 0.2; // 20%
 
-// Task 1: Implement getPrices()
+// Task 1: getPrices
 function getPrices(taxBoolean) {
-    for (let i = 0; i < dishData.length; i++) {
-        let finalPrice;
+  if (typeof taxBoolean !== "boolean") {
+    console.log("You need to pass a boolean to the getPrices call!");
+    return;
+  }
 
-        if (taxBoolean === true) {
-            finalPrice = dishData[i].price * tax;
-        } else if (taxBoolean === false) {
-            finalPrice = dishData[i].price;
-        } else {
-            console.log("You need to pass a boolean to the getPrices call!");
-            return;
-        }
-
-        console.log("Dish:", dishData[i].name, "Price: $", finalPrice.toFixed(2));
-    }
-}
-
-// Task 2: Implement getDiscount()
-function getDiscount(taxBoolean, guests) {
-    getPrices(taxBoolean);
-
-    if (typeof guests === "number" && guests > 0 && guests < 30) {
-        let discount = 0;
-
-        if (guests < 5) {
-            discount = 5;
-        } else if (guests >= 5) {
-            discount = 10;
-        }
-
-        console.log("Discount is: $", discount);
+  for (let i = 0; i < dishData.length; i++) {
+    let finalPrice;
+    if (taxBoolean) {
+      finalPrice = dishData[i].price + dishData[i].price * tax;
     } else {
-        console.log("The second argument must be a number between 0 and 30");
+      finalPrice = dishData[i].price;
     }
+
+    // <-- NOTICE: no comma in the string
+    console.log("Dish: " + dishData[i].name + " Price: $" + finalPrice.toFixed(2));
+  }
 }
 
-// Call getDiscount() with sample arguments
-getDiscount(true, 2);
-getDiscount(false, 10);
+// Task 2: getDiscount
+function getDiscount(taxBoolean, guests) {
+  getPrices(taxBoolean);
+
+  if (typeof guests !== "number" || guests <= 0 || guests >= 30) {
+    console.log("The second argument must be a number between 0 and 30");
+    return;
+  }
+
+  let discount = guests < 5 ? 5 : 10;
+  console.log("Discount is: $" + discount);
+}
+
+// Export functions for autograder
+module.exports = { getPrices, getDiscount };
